e31453bbd9820485921f689b36c8e6036dc889e4
longdang78 <longdang2309@gmail.com>
2020-08-09 23:05:03 +0700

    Update

--- Project1/graph.py
+++ Project1/graph.py
@@ -1,8 +1,8 @@
 from turtle import *
 from search import *
+import numpy as np
 square=24
-x_left_top=-150
-y_left_top=100
+
 scr=Screen()
 scr.bgcolor("black")
 scr.setup(1000,1000,0,0)
@@ -23,14 +23,13 @@ write("PROJECT 1",align="center" ,font=("Arial",50,"normal"))
 def input_shape():
     move=["d","u","r","l"]
     wall_shape=r"Pic\wall.gif"
-    food_shape=[r"Pic\food"+str(i)+".gif" for i in range(1,4)]
-    monster_shape=[[r"Pic\monster_"+i+str(j)+".gif" for j in range(1,4)] for i in move]
+    food_shape=[r"Pic\food"+str(i)+".gif" for i in range(1,6)]
+    monster_shape=[[[r"Pic\monster"+str(k)+"_"+i+str(j)+".gif" for j in range(1,4)] for i in move]for k in range(1,3)]
     player_shape=[["Pic\\"+i+str(j)+".gif" for j in range(1,4)] for i in move]
-    win_effect=[r"Pic\win"+str(i)+".gif" for i in range(1,9)]
-    lose_effect=[r"Pic\lose"+str(i)+".gif" for i in range(1,10)]
-    return wall_shape,food_shape,monster_shape,player_shape,win_effect,lose_effect
+    win_effect=[["Pic\\"+str(j)+"win"+str(i)+".gif" for i in range(1,9)] for j in range(1,3)]
+    return wall_shape,food_shape,monster_shape,player_shape,win_effect
 
-wall_shape,food_shape,monster_shape,player_shape,win_effect,lose_effect=input_shape()
+wall_shape,food_shape,monster_shape,player_shape,win_effect=input_shape()
 
 
 
@@ -38,15 +37,16 @@ scr.addshape(wall_shape)
 for i in food_shape:
     scr.addshape(i)
 for i in win_effect:
-    scr.addshape(i)
+    for j in i:
+        scr.addshape(j)
 for i in player_shape:
     for j in i:
         scr.addshape(j)
 for i in monster_shape:
     for j in i:
-        scr.addshape(j)
-for i in lose_effect:
-    scr.addshape(i)
+        for k in j:
+            scr.addshape(k)
+
 def graphic_loadmap(fi):
     map_game=[]
     food=[]
@@ -74,16 +74,24 @@ def graphic_loadmap(fi):
         for x in line.split():
             pac_man.append(int(x))
         f.close()
-    return map_game,food,monster,pac_man,n,m
+    global x_left_top,y_left_top
+    x_left_top= -((m//2)+1)*24
+    y_left_top=(n//2 + 1)*24
+
+
+    return map_game,np.array(food),np.array(monster),np.array(pac_man),n,m
 
 
 class Pen(Turtle):
-    def __init__(self):
+    def __init__(self,wall):
         Turtle.__init__(self)
         self.color("white")
         self.speed(0)
         self.penup()
-    def draw_outline(self,h,w,x0,y0):
+        self.shape(wall)
+    def draw_outline(self,h,w):
+        x0= -((w//2)+1)*square
+        y0=(h//2 + 1)*square
         for i in range(w+2):
             self.goto(x0+i*square,y0)
             self.stamp()
@@ -96,11 +104,10 @@ class Pen(Turtle):
         for i in range(1,h+1):
             self.goto(x0,y0-i*square)
             self.stamp()
-    def draw_map(self,map,h,w,x0,y0,shape):
-        self.shape(wall_shape)
-        self.draw_outline(h,w,x0,y0)
-        x=x0+square
-        y=y0-square
+    def draw_map(self,map,h,w):
+        self.draw_outline(h,w)
+        x=-((w//2)+1)*square+square
+        y=(h//2 + 1)*square-square
         food=[]
         monster=[]
         for i in range(h):
@@ -133,27 +140,27 @@ class Player(Turtle):
         self.speed(4)
     def down(self):
         self.seth(270)
-        for i in range(8):
+        for i in range(12):
             self.shape(self.s[0][i%2])
-            self.fd(3)
+            self.fd(2)
         self.shape(self.s[0][2])
     def up(self):
         self.seth(90)
-        for i in range(8):
+        for i in range(12):
             self.shape(self.s[1][i%2])
-            self.fd(3)
+            self.fd(2)
         self.shape(self.s[1][2])
     def right(self):
         self.seth(0)
-        for i in range(8):
+        for i in range(12):
             self.shape(self.s[2][i%2])
-            self.fd(3)
+            self.fd(2)
         self.shape(self.s[2][2])
     def left(self):
         self.seth(180)
-        for i in range(8):
+        for i in range(12):
             self.shape(self.s[3][i%2])
-            self.fd(3)
+            self.fd(2)
         self.shape(self.s[3][2])
     def move(self,index):
         if (self.row==index[0] and self.col>index[1]):
@@ -166,24 +173,27 @@ class Player(Turtle):
             self.up()
         self.row,self.col=index
 class Food(Turtle):
-    def __init__(self,s,effect):
+    def __init__(self,s,effect,x,y):
         Turtle.__init__(self)
         self.ht()
         self.speed(0)
         self.penup()
         self.s=s
         self.effect=effect
+        self.x=x
+        self.y=y
     def start(self,x,y):
-        self.shape(self.s[0])
+        self.shape(self.s)
         self.goto(x,y)
         self.st()
         self.speed(5)   
-    def win_effect(self):
+    def win_effect(self,index):
         for _ in range(2):
-            for i in self.effect:
-                scr.delay(50)
+            for i in self.effect[index%2]:
+                scr.delay(25)
                 self.shape(i)
         self.ht()
+
 
 
     
--- Project1/main.py
+++ Project1/main.py
@@ -4,19 +4,24 @@ from graph import *
 file_name=['map1.1.txt','map1.2.txt','map2.1.txt','map2.2.txt']
 #123123123
 
-map,food,monster,pacman,h,w=graphic_loadmap(file_name[2])   
-a=Pen()
-x0,y0=a.draw_map(map,h,w,x_left_top,y_left_top,shape)
+map,food,monster,pacman,h,w=graphic_loadmap("15x20.txt")
+a=Pen(wall_shape)
+x0,y0=a.draw_map(map,h,w)
 p=Player(player_shape,pacman[0],pacman[1])
 p.start(x0+pacman[1]*square,y0-pacman[0]*square)
-m=Player(monster_shape,monster[0][0],monster[0][1])
-m.start(x0+monster[0][1]*square,y0-monster[0][0]*square)
-f1=Food(food_shape,win_effect)
-f1.start(x0+food[0][1]*square,y0-food[0][0]*square)
+m=[Player(monster_shape[i%2],monster[i][0],monster[i][1]) for i in range(len(monster))]
+for i in range(len(m)):
+    m[i].start(x0+monster[i][1]*square,y0-monster[i][0]*square)
+f=[Food(food_shape[i%5],win_effect,food[i][1],food[i][0])for i in range(len(food))]
+for i in range(len(f)):
+    f[i].start(x0+food[i][1]*24,y0-food[i][0]*24)
 
-path=Lv12_A_Star(file_name[2])
+path=Lv12_A_Star("15x20.txt")
 if (type(path)==list):
     for i in path:
         p.move(i)
-f1.win_effect()
+        list_eaten=np.argwhere((food==np.array([p.row,p.col])).prod(axis=1)==1)
+        if list_eaten.size!=0:
+           f[list_eaten.item()].win_effect(np.random.randint(1,2))
+
 done()
