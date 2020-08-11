449bc09dc8a5be3fa202b9dcadb483f3df479dcf
longdang78 <longdang2309@gmail.com>
2020-08-11 23:55:49 +0700

    update

--- Project1/graph.py
+++ Project1/graph.py
@@ -1,6 +1,7 @@
 from turtle import *
 from search import *
 import numpy as np
+from time import *
 square=24
 
 scr=Screen()
@@ -12,13 +13,6 @@ setpos(0,300)
 color("white")
 write("PROJECT 1",align="center" ,font=("Arial",50,"normal"))
 
-#food_shape=[r"Pic\food1.gif",r"Pic\food2.gif",r"Pic\food3.gif"]
-#wall_shape=r"Pic\wall.gif"
-#monster_shape=[[r"Pic\monster_d1.gif",r"Pic\monster_d2.gif",r"Pic\monster_d3.gif"],[r"Pic\monster_u1.gif",r"Pic\monster_u2.gif",r"Pic\monster_u3.gif"],[r"Pic\monster_r1.gif",r"Pic\monster_r2.gif",r"Pic\monster_r3.gif"],[r"Pic\monster_l1.gif",r"Pic\monster_l2.gif",r"Pic\monster_l3.gif"]]
-#player_shape=[[r"Pic\d1.gif",r"Pic\d2.gif",r"Pic\d3.gif"],[r"Pic\u1.gif",r"Pic\u2.gif",r"Pic\u3.gif"],[r"Pic\r1.gif",r"Pic\r2.gif",r"Pic\r3.gif"],[r"Pic\l1.gif",r"Pic\l2.gif",r"Pic\l3.gif"]]
-#win_effect=[r"Pic\win1.gif",r"Pic\win2.gif",r"Pic\win3.gif",r"Pic\win4.gif",r"Pic\win5.gif",r"Pic\win6.gif",r"Pic\win7.gif",r"Pic\win8.gif"]
-#lose_effect=[r"Pic\lose1.gif",r"Pic\lose2.gif",r"Pic\lose3.gif",r"Pic\lose4.gif",r"Pic\lose5.gif",r"Pic\lose6.gif",r"Pic\lose7.gif",r"Pic\lose8.gif",r"Pic\lose9.gif"]
-
 
 def input_shape():
     move=["d","u","r","l"]
@@ -27,9 +21,10 @@ def input_shape():
     monster_shape=[[[r"Pic\monster"+str(k)+"_"+i+str(j)+".gif" for j in range(1,4)] for i in move]for k in range(1,3)]
     player_shape=[["Pic\\"+i+str(j)+".gif" for j in range(1,4)] for i in move]
     win_effect=[["Pic\\"+str(j)+"win"+str(i)+".gif" for i in range(1,9)] for j in range(1,3)]
-    return wall_shape,food_shape,monster_shape,player_shape,win_effect
+    lose_effect=[r"Pic\lose"+ str(i)+".gif" for i in range(1,9)]
+    return wall_shape,food_shape,monster_shape,player_shape,win_effect,lose_effect
 
-wall_shape,food_shape,monster_shape,player_shape,win_effect=input_shape()
+wall_shape,food_shape,monster_shape,player_shape,win_effect,lose_effect=input_shape()
 
 
 
@@ -46,7 +41,8 @@ for i in monster_shape:
     for j in i:
         for k in j:
             scr.addshape(k)
-
+for i in lose_effect:
+    scr.addshape(i)
 def graphic_loadmap(fi):
     map_game=[]
     food=[]
@@ -58,7 +54,6 @@ def graphic_loadmap(fi):
         n=int(temp[0])
         m=int(temp[1])
         temp.clear()
-
         for row in range(n):
             line = f.readline()
             for number,y in zip(line.split(),range(m)):
@@ -85,7 +80,6 @@ def graphic_loadmap(fi):
 class Pen(Turtle):
     def __init__(self,wall):
         Turtle.__init__(self)
-        self.color("white")
         self.speed(0)
         self.penup()
         self.shape(wall)
@@ -110,15 +104,18 @@ class Pen(Turtle):
         y=(h//2 + 1)*square-square
         food=[]
         monster=[]
+        list_wall={}
+        self.ht()
         for i in range(h):
             for j in range(w):
                 if map[i][j]==1:
-                    temp=self.shape(wall_shape)
-                    self.goto(x+j*square,y-i*square)
-                    self.stamp()
+                    temp=self.clone()
+                    list_wall[(i,j)]=temp
+                    temp.goto(x+j*square,y-i*square)
+   
                 else:
                     continue
-        return x,y
+        return x,y,list_wall
 
 
 
@@ -172,6 +169,12 @@ class Player(Turtle):
         elif (self.row>index[0] and self.col==index[1]):
             self.up()
         self.row,self.col=index
+    def lose(self,s):
+        scr.delay(75)
+        for i in range(len(s)):
+                self.shape(s[i])
+        scr.delay(0)
+        self.ht()
 class Food(Turtle):
     def __init__(self,s,effect,x,y):
         Turtle.__init__(self)
@@ -185,15 +188,76 @@ class Food(Turtle):
     def start(self,x,y):
         self.shape(self.s)
         self.goto(x,y)
-        self.st()
         self.speed(5)   
     def win_effect(self,index):
+        scr.delay(25)
         for _ in range(2):
             for i in self.effect[index%2]:
-                scr.delay(25)
                 self.shape(i)
+        scr.delay(0)
         self.ht()
 
+class Map:
+    def __init__(self, filename):
+        self.map,self.food,monster,pacman,self.h,self.w=graphic_loadmap(filename)
+        a=Pen(wall_shape)
+        x0,y0,self.list_wall=a.draw_map(self.map,self.h,self.w)
+        self.p=Player(player_shape,pacman[0],pacman[1])
+        self.m=[Player(monster_shape[i%2],monster[i][0],monster[i][1]) for i in range(len(monster))]
+        self.f=[Food(food_shape[i%5],win_effect,self.food[i][0],self.food[i][1])for i in range(len(self.food))]
+        for i in range(len(self.m)):
+            self.m[i].start(x0+monster[i][1]*square,y0-monster[i][0]*square)
+            self.list_wall[(monster[i][0],monster[i][1])]=self.m[i]
+        for i in range(len(self.f)):
+            self.f[i].start(x0+self.food[i][1]*24,y0-self.food[i][0]*24)
+            self.list_wall[(self.food[i][0],self.food[i][1])]=self.f[i]
+        self.p.start(x0+pacman[1]*square,y0-pacman[0]*square)
+        self.p.st()
+    def eat_food(self):
+        list_eaten=np.argwhere((self.food==np.array([self.p.row,self.p.col])).prod(axis=1)==1)
+        if list_eaten.size!=0:
+            self.p.ht()
+            self.f[list_eaten.item()].win_effect(np.random.randint(1,2))
+            self.p.st()
+    def explore(self):
+        a,b,c,d=self.p.row-2 if self.p.row-2>=0 else 0,self.p.row+3 if self.p.row+3<=self.h else self.h,self.p.col-2 if self.p.col-2>=0 else 0,self.p.col+3 if self.p.col+3<=self.w else self.w
+        temp=[(x,y) for x in range(a,b) for y in range(c,d)]
+        for i in temp:
+                if i in self.list_wall:
+                    self.list_wall.pop(i).st()
+                    
+    def show_all(self):
+        for i in self.list_wall:
+            i.st()
+    def move(self,path):
+        if (type(path)==list):
+            for i in path:
+                self.p.move(i)
+                self.eat_food()
 
+    def level1_2(self,path):
+        self.show_all()
+        if (type(path)==list):
+            for i in path:
+                self.p.move(i)
+                self.eat_food()
+        else:
+            scr.delay(100)
+            for i in range(4):
+                self.p.shape(self.p.s[i][2])
+            scr.delay(0)
+            self.p.lose(lose_effect)
 
-    
+    def level3(self,path):
+        self.explore()
+        if (type(path)==list):
+            for i in path:
+                self.p.move(i)
+                self.explore()
+                self.eat_food()
+        else:
+            scr.delay(100)
+            for i in range(4):
+                self.p.shape(self.p.s[i][2])
+            scr.delay(0)
+            self.p.lose(lose_effect)
--- Project1/main.py
+++ Project1/main.py
@@ -3,25 +3,9 @@ from graph import *
 
 file_name=['map1.1.txt','map1.2.txt','map2.1.txt','map2.2.txt']
 #123123123
+map=Map("map1.1.txt")
 
-map,food,monster,pacman,h,w=graphic_loadmap("15x20.txt")
-a=Pen(wall_shape)
-x0,y0=a.draw_map(map,h,w)
-p=Player(player_shape,pacman[0],pacman[1])
-p.start(x0+pacman[1]*square,y0-pacman[0]*square)
-m=[Player(monster_shape[i%2],monster[i][0],monster[i][1]) for i in range(len(monster))]
-for i in range(len(m)):
-    m[i].start(x0+monster[i][1]*square,y0-monster[i][0]*square)
-f=[Food(food_shape[i%5],win_effect,food[i][1],food[i][0])for i in range(len(food))]
-for i in range(len(f)):
-    f[i].start(x0+food[i][1]*24,y0-food[i][0]*24)
 
-path=Lv12_A_Star("15x20.txt")
-if (type(path)==list):
-    for i in path:
-        p.move(i)
-        list_eaten=np.argwhere((food==np.array([p.row,p.col])).prod(axis=1)==1)
-        if list_eaten.size!=0:
-           f[list_eaten.item()].win_effect(np.random.randint(1,2))
-
+path=Lv12_A_Star("map1.1.txt")
+map.level3(path)
 done()
