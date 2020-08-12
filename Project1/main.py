63979e1d3b8f2375cb00efcb308f883523c60fdc
longdang78 <longdang2309@gmail.com>
2020-08-12 10:02:40 +0700

    Update

--- Project1/main.py
+++ Project1/main.py
@@ -4,8 +4,10 @@ from graph import *
 file_name=['map1.1.txt','map1.2.txt','map2.1.txt','map2.2.txt']
 #123123123
 map=Map("map1.1.txt")
-
-
 path=Lv12_A_Star("map1.1.txt")
-map.level3(path)
+# level 1 và level 2 thì dùng hàm này
+#map.level1_2(path)
+# level 3 thì dùng hàm này
+#map.level3(path)
+
 done()
