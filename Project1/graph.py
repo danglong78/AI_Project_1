34d93675e993c4b245a258ab0b3e833f1f3f553a
longdang78 <longdang2309@gmail.com>
2020-08-29 22:01:03 +0700

    Update nhá

--- Project1/graph.py
+++ Project1/graph.py
@@ -1,48 +1,67 @@
 from turtle import *
-from search import *
 import numpy as np
 from time import *
-square=24
+square=48
 
 scr=Screen()
-scr.bgcolor("black")
+scr.bgcolor("#f0f5f0")
 scr.setup(1000,1000,0,0)
 ht()
 penup()
 setpos(0,300)
-color("white")
-write("PROJECT 1",align="center" ,font=("Arial",50,"normal"))
+color("black")
+write("project 1",align="center" ,font=("arial",50,"normal"))
 
 
 def input_shape():
     move=["d","u","r","l"]
-    wall_shape=r"Pic\wall.gif"
-    food_shape=[r"Pic\food"+str(i)+".gif" for i in range(1,6)]
-    monster_shape=[[[r"Pic\monster"+str(k)+"_"+i+str(j)+".gif" for j in range(1,4)] for i in move]for k in range(1,3)]
-    player_shape=[["Pic\\"+i+str(j)+".gif" for j in range(1,4)] for i in move]
-    win_effect=[["Pic\\"+str(j)+"win"+str(i)+".gif" for i in range(1,9)] for j in range(1,3)]
-    lose_effect=[r"Pic\lose"+ str(i)+".gif" for i in range(1,9)]
-    return wall_shape,food_shape,monster_shape,player_shape,win_effect,lose_effect
+    cell_shape=r"Pic\cell.gif"
+    breeze_shape=r"Pic\breeze.gif"
+    stench_shape=r"Pic\stench.gif"
+    bs_shape=r"Pic\BS.gif"
+    bg_shape=r"Pic\bg.gif"
+    bsg_shape=r"Pic\bsg.gif"
+    sg_shape=r"Pic\sg.gif"
+    wall_shape=r"Pic\floor.gif"
+    pit_shape=r"Pic\pit.gif"
+    gold_shape=r"Pic\gold_floor.gif"
+    wumpus_shape="Pic\wumpus.gif"
+    player_shape=["Pic\\"+i+".gif"  for i in move]
+    shoot_shape=["Pic\\s"+i+".gif"  for i in move]
+    return wall_shape,gold_shape,shoot_shape,player_shape,cell_shape,pit_shape,breeze_shape,stench_shape,bs_shape, wumpus_shape,bg_shape,bsg_shape,sg_shape
 
-wall_shape,food_shape,monster_shape,player_shape,win_effect,lose_effect=input_shape()
-
-
-
+wall_shape,gold_shape,shoot_shape,player_shape,cell_shape,pit_shape,breeze_shape,stench_shape,bs_shape, wumpus_shape,bg_shape,bsg_shape,sg_shape=input_shape()
+scr.addshape(wumpus_shape)
+scr.addshape(breeze_shape)
+scr.addshape(stench_shape)
+scr.addshape(bs_shape)
+scr.addshape(bg_shape)
+scr.addshape(bsg_shape)
+scr.addshape(sg_shape)
+scr.addshape(cell_shape)
+scr.addshape(pit_shape)
 scr.addshape(wall_shape)
-for i in food_shape:
-    scr.addshape(i)
-for i in win_effect:
-    for j in i:
-        scr.addshape(j)
+scr.addshape(gold_shape)
+
+for i in shoot_shape:
+        scr.addshape(i)
 for i in player_shape:
-    for j in i:
-        scr.addshape(j)
-for i in monster_shape:
-    for j in i:
-        for k in j:
-            scr.addshape(k)
-for i in lose_effect:
-    scr.addshape(i)
+        scr.addshape(i)
+
+all_shape={
+    'W':wumpus_shape,
+    'S':stench_shape,
+    'B':breeze_shape,
+    'P':pit_shape,
+    'BS':bs_shape,'SB':bs_shape,
+    'BSG':bsg_shape,'BGS':bsg_shape,'GSB':bsg_shape,'GBS':bsg_shape,'SGB':bsg_shape,'SBG':bsg_shape,
+    'SG':sg_shape,'GS':sg_shape,
+    'BG':bg_shape,'GB':bg_shape,
+    '-':wall_shape,
+    'G':gold_shape,
+    'A':wall_shape
+    }
+  #---------------------------------------------
 def graphic_loadmap(fi):
     map_game=[]
     food=[]
@@ -50,32 +69,17 @@ def graphic_loadmap(fi):
     f = open(fi, "r")
     if f.mode == "r":
         line =f.readline()
-        temp=line.split()
-        n=int(temp[0])
-        m=int(temp[1])
-        temp.clear()
+        n=int(line)
         for row in range(n):
-            line = f.readline()
-            for number,y in zip(line.split(),range(m)):
-                temp.append(int(number))
-                if int(number)==2:
-                    food.append([row,y])
-                if int(number)==3:
-                    monster.append([row,y])
-            map_game.append(temp.copy())
-            temp.clear()
-        line =f.readline()
-        pac_man=[]
-        for x in line.split():
-            pac_man.append(int(x))
+            line = f.readline().strip('\n').split('.')
+            map_game.append(line)
+        map_game=np.array(map_game)
+        start_postion=[int(i) for i in np.where(map_game=='A')]     
         f.close()
     global x_left_top,y_left_top
-    x_left_top= -((m//2)+1)*24
-    y_left_top=(n//2 + 1)*24
-
-
-    return map_game,np.array(food),np.array(monster),np.array(pac_man),n,m
-
+    x_left_top= -((n//2))*square
+    y_left_top=(n//2)*square
+    return map_game,n,start_postion
 
 class Pen(Turtle):
     def __init__(self,wall):
@@ -83,41 +87,31 @@ class Pen(Turtle):
         self.speed(0)
         self.penup()
         self.shape(wall)
-    def draw_outline(self,h,w):
-        x0= -((w//2)+1)*square
-        y0=(h//2 + 1)*square
-        outline_stamp=[]
-        for i in range(w+2):
-            self.goto(x0+i*square,y0)
-            outline_stamp+=[self.stamp()]
-        for i in range(1,h+2):
-            self.goto(x0+(w+1)*square,y0-i*square)
-            outline_stamp+=[self.stamp()]
-        for i in range(w+1):
-            self.goto(x0+i*square,y0-(h+1)*square)
-            outline_stamp+=[self.stamp()]
-        for i in range(1,h+1):
-            self.goto(x0,y0-i*square)
-            outline_stamp+=[self.stamp()]
-        return outline_stamp
+        self.s=""
     def draw_map(self,map,h,w):
-        outline_stamp=self.draw_outline(h,w)
-        x=-((w//2)+1)*square+square
-        y=(h//2 + 1)*square-square
-        food=[]
+        x=-((w//2))*square
+        y=(h//2)*square
+        gold=[]
         monster=[]
-        list_wall={}
         self.st()
+        list_wall={(x,y):self.clone() for x in range(h) for y in range(w)}
+        
         for i in range(h):
             for j in range(w):
-                if map[i][j]==1:
-                    temp=self.clone()
-                    list_wall[(i,j)]=temp
+                if map[i][j]!='-' and map[i][j] !='A':
+                    temp=list_wall[(i,j)]
+                    temp.shape(all_shape[map[i][j]])
+                    temp.s=all_shape[map[i][j]]
                     temp.goto(x+j*square,y-i*square)
-   
+           
                 else:
-                    continue
-        return x,y,list_wall,outline_stamp
+                    #()
+                    temp= list_wall[(i,j)]
+                    temp.s=wall_shape
+                    temp.shape(wall_shape)
+                    temp.goto(x+j*square,y-i*square) 
+        self.ht()
+        return x,y,list_wall
 
 
 
@@ -126,40 +120,32 @@ class Pen(Turtle):
 class Player(Turtle):
     def __init__(self,s,row,col):
         Turtle.__init__(self)
-        self.speed(0)
+        self.speed("slow")
         self.ht()
         self.s=s
         self.penup()
         self.row=row
         self.col=col
     def start(self,x,y):
-        self.shape(self.s[0][2])
+        self.shape(self.s[0])
         self.goto(x,y)
         self.st()
     def down(self):
         self.seth(270)
-        for i in range(4):
-            self.shape(self.s[0][i%2])
-            self.fd(6)
-        self.shape(self.s[0][2])
+        self.shape(self.s[0])
+        self.fd(48)
     def up(self):
         self.seth(90)
-        for i in range(4):
-            self.shape(self.s[1][i%2])
-            self.fd(6)
-        self.shape(self.s[1][2])
+        self.shape(self.s[1])
+        self.fd(48)
     def right(self):
         self.seth(0)
-        for i in range(4):
-            self.shape(self.s[2][i%2])
-            self.fd(6)
-        self.shape(self.s[2][2])
+        self.shape(self.s[2])
+        self.fd(48)
     def left(self):
         self.seth(180)
-        for i in range(4):
-            self.shape(self.s[3][i%2])
-            self.fd(6)
-        self.shape(self.s[3][2])
+        self.shape(self.s[3])
+        self.fd(48)
     def move(self,index):
         if (self.row==index[0] and self.col>index[1]):
             self.left()
@@ -170,145 +156,149 @@ class Player(Turtle):
         elif (self.row>index[0] and self.col==index[1]):
             self.up()
         self.row,self.col=index
-    def lose(self,s):  
-        for i in range(len(s)):
-                self.shape(s[i])
-                sleep(0.01)
-        self.ht()
-class Food(Turtle):
-    def __init__(self,s,effect,x,y):
-        Turtle.__init__(self)
-        self.ht()
-        self.speed(0)
-        self.penup()
-        self.s=s
-        self.effect=effect
-        self.x=x
-        self.y=y
-    def start(self,x,y):
-        self.shape(self.s)
-        self.goto(x,y)
-        self.st()
-        self.speed(5)   
-    def win_effect(self,index):
-        for _ in range(2):
-            for i in self.effect[index%2]:
-                self.shape(i)
-                sleep(0.01)
-        self.ht()
+    def shoot(self,s,index):
+        if (self.row==index[0] and self.col>index[1]):
+            self.shape(s[3])
+        elif (self.row==index[0] and self.col<index[1]):
+            self.shape(s[2])
+        elif (self.row<index[0] and self.col==index[1]):
+            self.shape(s[0])
+        elif (self.row>index[0] and self.col==index[1]):
+            self.shape(s[1])
+        sleep(0.75)
 
 class Map:
     def __init__(self, filename):
-        self.map,self.food,monster,pacman,self.h,self.w=graphic_loadmap(filename)
+        self.map,self.n,player=graphic_loadmap(filename)
         self.outline=Pen(wall_shape)
-        x0,y0,self.list_wall,self.outline_stamp=self.outline.draw_map(self.map,self.h,self.w)
-        self.p=Player(player_shape,pacman[0],pacman[1])
-        self.m=[Player(monster_shape[i%2],monster[i][0],monster[i][1]) for i in range(len(monster))]
-        self.f=[Food(food_shape[i%5],win_effect,self.food[i][0],self.food[i][1])for i in range(len(self.food))]
-        for i in range(len(self.m)):
-            self.m[i].start(x0+monster[i][1]*square,y0-monster[i][0]*square)
-            self.list_wall[(monster[i][0],monster[i][1])]=self.m[i]
-        for i in range(len(self.f)):
-            self.f[i].start(x0+self.food[i][1]*24,y0-self.food[i][0]*24)
-            self.list_wall[(self.food[i][0],self.food[i][1])]=self.f[i]
-        self.p.start(x0+pacman[1]*square,y0-pacman[0]*square)
-        self.p.st()
+        x0,y0,self.list_wall,=self.outline.draw_map(self.map,self.n,self.n)
+        self.p=Player(player_shape,player[0],player[1])
+        self.p.start(x0+player[1]*square,y0-player[0]*square)
         self.eaten=0
         self.score=0
-    def eat_food(self):
-        list_eaten=np.argwhere((self.food==np.array([self.p.row,self.p.col])).prod(axis=1)==1)
-        if list_eaten.size!=0:
-            self.p.ht()
-            self.f[list_eaten.item(0)].win_effect(np.random.randint(1,2))
-            self.food[list_eaten.item(0)]=[-1,-1]
-            self.p.st()
-            self.eaten+=1
-            self.score+=20
+        self.hide_all()
+        self.explore()
 
     def is_beat(self):
-        monster=[(i.row,i.col) for i in self.m]
-        list_beaten=np.argwhere((monster==np.array([self.p.row,self.p.col])).prod(axis=1)==1)
-        if list_beaten.size!=0:
-           self.m[list_beaten.item(0)].ht()
-           self.p.lose(lose_effect)
-           self.m[list_beaten.item(0)].st()
-           return True
+        if self.map[self.p.row][self.p.col] in ['W','P','WG','GW']:
+            return True
         return False
 
     def explore(self):
-        a,b,c,d=self.p.row-2 if self.p.row-2>=0 else 0,self.p.row+3 if self.p.row+3<=self.h else self.h,self.p.col-2 if self.p.col-2>=0 else 0,self.p.col+3 if self.p.col+3<=self.w else self.w
-        temp=[(x,y) for x in range(a,b) for y in range(c,d)]
-        for i in temp:
-            if i in self.list_wall:
-                self.list_wall[i].st()
-                if type(self.list_wall[i])==Food:
-                    self.list_wall.pop(i)
-    def monster_visual(self):
-        a,b,c,d=self.p.row-2 if self.p.row-2>=0 else 0,self.p.row+3 if self.p.row+3<=self.h else self.h,self.p.col-2 if self.p.col-2>=0 else 0,self.p.col+3 if self.p.col+3<=self.w else self.w
-        temp=[(x,y) for x in range(a,b) for y in range(c,d)]
-        pos=[(i.row,i.col,i) for i in self.m]
-        for i in pos:
-            if i not in temp:
-                i[2].ht()
+        i,j=self.p.row,self.p.col
+        self.list_wall[(i,j)].shape(self.list_wall[(i,j)].s)
+
     def hide_all(self):
         for i in self.list_wall.values():
-            i.ht()
+            i.shape(cell_shape)
+    def eat_gold(self):
+        i,j=self.p.row,self.p.col
+        if self.list_wall[i,j].s==gold_shape:
+            self.map[i][j]='-'
+            self.list_wall[(i,j)].shape(wall_shape)
+            self.list_wall[(i,j)].s=wall_shape
+        elif self.list_wall[i,j].s==sg_shape:
+            self.map[i][j]='S'
+            self.list_wall[(i,j)].shape(stench_shape)
+            self.list_wall[(i,j)].s=stench_shape
+        elif self.list_wall[i,j].s==bsg_shape:
+            self.map[i][j]='BS'
+            self.list_wall[(i,j)].shape(bs_shape)
+            self.list_wall[(i,j)].s=bs_shape
+        elif self.list_wall[i,j].s==bg_shape:
+            self.map[i][j]='B'
+            self.list_wall[(i,j)].shape(breeze_shape)
+            self.list_wall[(i,j)].s=breeze_shape
 
-
-    def level1_2(self,path):
-        if (type(path)==list):
-            for i in path:
+            
+    def game(self,step):
+        for i in step:
+            if len(i)==2:
                 self.p.move(i)
-                self.score-=1
-                self.eat_food()
-            return True
-        else:
-            self.p.lose(lose_effect)
-            return False
-
-    def level3_4_hide(self,path_pacman,path_monster):
-        self.hide_all()
-        self.explore()
-        if (type(path_pacman)==list):
-            for i in range(len(path_pacman)):
-                self.p.move(path_pacman[i])
-                self.score-=1
-                self.eat_food()
-                if  self.eaten==len(self.f):
-                    return True
-                for j in range(len(self.m)):
-                    self.m[j].move(path_monster[j][i])
-                if self.is_beat():
-                    return False
                 self.explore()
-    def level3_4_show(self,path_pacman,path_monster):
- 
-        if (type(path_pacman)==list):
-            for i in range(len(path_pacman)):
-                self.p.move(path_pacman[i])
                 if self.is_beat():
-                    return False
-                self.score-=1
-                self.eat_food()
-                if  self.eaten==len(self.f):
-                    return True
-                for j in range(len(self.m)):
-                    self.m[j].move(path_monster[j][i])
-                if self.is_beat():
-                    return False
-    def print_result(self,isWin):
-        self.outline.clearstamps(len(self.outline_stamp))
-        self.hide_all()
+                    break
+                self.eat_gold()
+            elif len(i)==3:
+                self.p.shoot(shoot_shape,(i[1],i[2]))
+                self.remove_wumpus(i[1],i[2])
+    def remove_wumpus(self,i,j):
+        if self.map[i][j]=='WG' or self.map[i][j]=='GW' :
+            self.map[i][j]='G'
+            self.list_wall[(i,j)].shape(gold_shape)
+            self.list_wall[(i,j)].s=gold_shape
+            surround=get_surround(self.n,self.n,i,j)
+            for (s1,s2) in surround:
+                if self.list_wall[(s1,s2)].s==stench_shape:
+                    self.map[s1][s2]='-'
+                    self.list_wall[(s1,s2)].shape(wall_shape)
+                    self.list_wall[(s1,s2)].s=wall_shape
+                elif self.list_wall[(s1,s2)].s==bsg_shape:
+                    self.map[s1][s2]='BG'               
+                    self.list_wall[(s1,s2)].shape(bg_shape)
+                    self.list_wall[(s1,s2)].s=bg_shape
+                elif self.list_wall[(s1,s2)].s==bs_shape:
+                    self.map[s1][s2]='B'               
+                    self.list_wall[(s1,s2)].shape(breeze_shape)
+                    self.list_wall[(s1,s2)].s=breeze_shape
+                elif self.list_wall[(s1,s2)].s==sg_shape:
+                    self.map[s1][s2]='G'
+                    self.list_wall[(s1,s2)].shape(gold_shape)
+                    self.list_wall[(s1,s2)].s=gold_shape
+        elif self.map[i][j]=='W':
+            self.list_wall[(i,j)].shape(wall_shape)
+            self.list_wall[(i,j)].s=wall_shape
+            self.map[i][j]='-'
+            surround=get_surround(self.n,self.n,i,j)
+            for (s1,s2) in surround:
+                if self.list_wall[(s1,s2)].s==stench_shape:
+                    self.map[s1][s2]='-'
+                    self.list_wall[(s1,s2)].shape(wall_shape)
+                    self.list_wall[(s1,s2)].s=wall_shape
+                elif self.list_wall[(s1,s2)].s==bsg_shape:
+                    self.map[s1][s2]='BG'               
+                    self.list_wall[(s1,s2)].shape(bg_shape)
+                    self.list_wall[(s1,s2)].s=bg_shape
+                elif self.list_wall[(s1,s2)].s==bs_shape:
+                    self.map[s1][s2]='B'               
+                    self.list_wall[(s1,s2)].shape(breeze_shape)
+                    self.list_wall[(s1,s2)].s=breeze_shape
+                elif self.list_wall[(s1,s2)].s==sg_shape:
+                    self.map[s1][s2]='G'
+                    self.list_wall[(s1,s2)].shape(gold_shape)
+                    self.list_wall[(s1,s2)].s=gold_shape
+
+
+
+    
+
+    def print_result(self):
+        scr.clear()
+        scr.bgcolor("#f0f5f0")
+        scr.setup(1000,1000,0,0)
+        ht()
+        penup()
+        setpos(0,300)
+        color("black")
+        write("PROJECT 1",align="center" ,font=("Arial",50,"normal"))
         self.p.ht()
         self.outline.ht()
-        output=""
-        if isWin:
-            output="YOU WIN"
-        else:
-            output="YOU LOSE"
         penup()
-        setpos(-50,50)
-        color("yellow")
-        write(output,align="center" ,font=("STCaiyun",60,"normal"))
-        setpos(-50,0)
+        setpos(0,50)
+        color("black")
+        setpos(0,0)
         write("SCORE: "+str(self.score),align="center" ,font=("Arial",40,"normal"))
+
+
+def get_surround(h,w,i,j):
+    rs=[]
+    if i-1>=0:
+        rs.append((i-1,j))
+    if j-1>=0:
+        rs.append((i,j-1))
+    if i+1<h:
+        rs.append((i+1,j))
+    if j+1<w:
+        rs.append((i,j+1))
+    return rs
+
--- Project1/main.py
+++ Project1/main.py
@@ -1,28 +1,7 @@
 from graph import *
-from level3_4 import *
-from search import *
-level=int(input("Mời bạn nhập level: "))
-result=False
-filename=input("Mời bạn nhập tên File input: ")
-map=Map(filename)
-if level==1 or level==2:
-    path=Lv12_A_Star(filename)
-    result=map.level1_2(path)
-if level==3:
-    [pm_path,ghost_path]=lv3(filename)
-    temp=int(input("Nhập 1 nếu bạn muốn bản độ hiện ra hết.\n Nhập 2 nếu bạn muốn bản đồ chỉ hiện phần Pacman thấy\n Mời bạn chọn:   "))
-    if temp==1:
-        result=map.level3_4_show(pm_path,ghost_path)
-    if temp==2:
-        result=map.level3_4_hide(pm_path,ghost_path)
+from Propositional_Logic import *
+path = Play_Game('Wumpus.txt')
+map=Map("Wumpus.txt")
+map.game(path)
 
-if level==4:
-    [pm_path,ghost_path]=lv4(filename)
-    temp=int(input("Nhập 1 nếu bạn muốn bản độ hiện ra hết.\n Nhập 2 nếu bạn muốn bản đồ chỉ hiện phần Pacman thấy\n Mời bạn chọn:   "))
-    if temp==1:
-        result=map.level3_4_show(pm_path,ghost_path)
-    if temp==2:
-        result=map.level3_4_hide(pm_path,ghost_path)
-# tra ve path cua pacman va list cac path cua moster
-map.print_result(result)
 done()
